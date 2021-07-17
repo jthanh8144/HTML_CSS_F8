4cf2031ca3132373723b482b177f32b90e73ebe1
jthanh8144 <thanhvo618@gmail.com>
2021-07-17 16:20:26 +0700

    modal

---
+++ Band/assets/js/main.js
@@ -0,0 +1,15 @@
+var modal = document.querySelector('.modal');
+
+var buyBtns = document.querySelectorAll('.tour-places__buy');
+buyBtns.forEach(buyBtn => {
+    buyBtn.onclick = (e) => {
+        modal.classList.add('active');
+    }
+});
+
+function removeClass() {
+    modal.classList.remove('active');
+}
+
+document.querySelector('.modal-btn-close').onclick = removeClass;
+document.querySelector(".modal-cancel").onclick = removeClass;
