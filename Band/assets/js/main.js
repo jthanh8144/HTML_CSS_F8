069ded75f01494e0c730ae87144c8a17fa6efbc7
jthanh8144 <thanhvo618@gmail.com>
2021-07-18 15:40:51 +0700

    subnav mobile

--- Band/assets/js/main.js
+++ Band/assets/js/main.js
@@ -34,3 +34,16 @@ $('.mobile-menu__btn').onclick = () => {
         $('.nav-item:nth-child(2)').style.borderTop = null;
     }
 }
+
+// close sub nav
+console.log();
+var subnav = $('.subnav');
+$('.nav-more').onclick = () => {
+    if (!subnav.classList.contains('subnav--active')) {
+        subnav.classList.add('subnav--active');
+        subnav.classList.remove('subnav--hidden');
+    } else {
+        subnav.classList.remove('subnav--active');
+        subnav.classList.add('subnav--hidden');
+    }
+}
